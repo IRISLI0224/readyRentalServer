79c990cae07de10e3df5df976847f6d2d5294312
Jessie Liu <hanyan.liu@outlook.com>
2021-11-13 21:35:38 +1100

    controller added

---
+++ src/controller/users.js
@@ -0,0 +1,24 @@
+// display all users
+exports.index = (req, res) => {
+  return res.status(200).send("Failed");
+};
+
+//update one user
+exports.update = (req, res) => {
+  return res.status(200).send("Failed");
+};
+
+// delete one user
+exports.destroy = (req, res) => {
+  return res.status(200).send("Failed");
+};
+
+//store one user
+exports.store = (req, res) => {
+  return res.status(200).send("Failed");
+};
+
+//display one user
+exports.show = (req, res) => {
+  return res.status(200).send("Failed");
+};
--- src/routes/v1/api/index.js
+++ src/routes/v1/api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = new express.Router();
+const userController = require("../../../controller/users");
 
-router.get("/", (req, res) => {
-  return res.status(200).send("Failed");
-});
+router.get("/users", userController.index);
+router.post("/users", userController.store);
+router.delete("/users", userController.destroy);
+router.put("/users", userController.update);
+router.get("/users/:id", userController.show);
 
 module.exports = router;
