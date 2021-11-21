67135679546d4d17a8b6dca9999dcdef0834184c
Chun Lee <chun.lee2605@gmail.com>
2021-11-21 19:28:33 +1100

    Gene Lee update

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,24 +1,28 @@
 // display all users
 exports.index = (req, res) => {
-  return res.status(200).send("Failed");
+  return res.status(200).send('Failed');
+};
+
+exports.myName = (req, res) => {
+  return res.status(200).json({ name: 'Gene' });
 };
 
 //update one user
 exports.update = (req, res) => {
-  return res.status(200).send("Failed");
+  return res.status(200).send('Failed');
 };
 
 // delete one user
 exports.destroy = (req, res) => {
-  return res.status(200).send("Failed");
+  return res.status(200).send('Failed');
 };
 
 //store one user
 exports.store = (req, res) => {
-  return res.status(200).send("Failed");
+  return res.status(200).send('Failed');
 };
 
 //display one user
 exports.show = (req, res) => {
-  return res.status(200).send("Failed");
+  return res.status(200).send('Failed');
 };
--- src/routes/v1/api/index.js
+++ src/routes/v1/api/index.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const userController = require("../../../controller/users");
 
 router.get("/users", userController.index);
+router.get("/gene", userController.myName);
 router.post("/users", userController.store);
 router.delete("/users", userController.destroy);
 router.put("/users", userController.update);
