c76af9945c83d869e2424a11e34a066b2bd243d7
Chun Lee <chun.lee2605@gmail.com>
2021-11-22 15:31:03 +1100

    Gene Lee update-eslin-v3

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -3,7 +3,6 @@ const router = new express.Router();
 const userController = require("../../../controller/users");
 
 router.get("/users", userController.index);
-router.get("/gene", userController.myName);
 router.post("/users", userController.store);
 router.delete("/users", userController.destroy);
 router.put("/users", userController.update);
