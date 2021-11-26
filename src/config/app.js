b04058b1e15dba3bfde4369e16ba82cd6478a37b
Jzeva <jzeva1213@gmail.com>
2021-11-26 16:09:14 +1000

    fix conflict

--- src/config/app.js
+++ src/config/app.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const dotenv = require('dotenv');
-=======
 /* eslint-disable global-require */
 // const dotenv = require('dotenv');
->>>>>>> 9c4f85bd26b12230b5cab5d922ae9f7999119628
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
