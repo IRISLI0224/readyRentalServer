6f91859d54fb59658a47c0ab34bc92b31729dee1
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2021-11-30 13:03:52 +0800

    add user

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,5 +1,18 @@
-// display all users 所有用户
-exports.index = (req, res) => res.status(200).json('all users');
+// display all users
+exports.index = (req, res) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  res.status(200).json([
+    {
+        name: 'Jessie',
+        email: 'jessie@gmail.com',
+        password: 'abc1223',
+      },
+      {
+        name: 'Yu',
+        email: 'yu@gmail.com',
+        Password: 'abc1224',
+      },
+  ]);
 
 // update one user
 exports.update = (req, res) => res.status(200).json('Failed');
