c8f079b847abfe85a420870fdba365b0e4e5a862
Jessie Liu <hanyan.liu@outlook.com>
2021-12-06 18:39:00 +1100

    db bug fixed

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 exports.connectToDB = () => {
   const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
   const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-  return mongoose.connect(connectionString, {});
+  return mongoose.connect(connectionString);
 };
