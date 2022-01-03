3e55f8b943b5342a5c429cb26623ff36e6500a65
AllenBoxinHao <allen7hao@gmail.com>
2022-01-03 22:14:48 +1100

    response message

--- src/controller/imageUpload.js
+++ src/controller/imageUpload.js
@@ -5,11 +5,12 @@ const unlinkFile = util.promisify(fs.unlink);
 const { uploadFile } = require('./../config/s3');
 
 exports.store = async (req, res) => {
-  const file = req.file;;
+  const file = req.file;
 
   // some place to apply filter to image
 
   const result = await uploadFile(file);
+  console.log(result);
   await unlinkFile(file.path);
-  res.send({ imageInfo: `/images/${result}` });
+  res.send({ imagePath: `${result.Location}` });
 };
