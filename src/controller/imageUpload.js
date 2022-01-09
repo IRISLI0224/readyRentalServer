2adc7081d239066ac022f2a318ca57852e17ef0a
AllenBoxinHao <allen7hao@gmail.com>
2022-01-09 19:30:12 +1100

    file merged

--- src/controller/imageUpload.js
+++ src/controller/imageUpload.js
@@ -1,8 +1,32 @@
+require('dotenv').config();
 const fs = require('fs');
 const util = require('util');
+const S3 = require('aws-sdk/clients/s3');
 const unlinkFile = util.promisify(fs.unlink);
 
-const { uploadFile } = require('./../config/s3');
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+// uploads a file to S3
+function uploadFile(file) {
+  const fileStream = fs.createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+}
 
 exports.store = async (req, res) => {
   const file = req.file;
