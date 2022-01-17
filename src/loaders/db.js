72ea1de777abc0bb13128a6e04c321a4da052432
Jessie Liu <hanyan.liu@outlook.com>
2022-01-17 19:54:20 +1100

    test

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
 exports.connectToDB = () => {
-  const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  const {
+    NODE_ENV,
+    DB_HOST_LOCAL,
+    DB_PORT,
+    DB_DATABASE_LOCAL,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_DATABASE,
+  } = process.env;
+
+  let connectionString;
+  if (NODE_ENV === 'production') {
+    connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`;
+  } else {
+    connectionString = `mongodb://${DB_HOST_LOCAL}:${DB_PORT}/${DB_DATABASE_LOCAL}`;
+  }
+
   return mongoose.connect(connectionString);
 };
