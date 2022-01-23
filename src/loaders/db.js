43b09f38a6d535b47cd473f97c65a7a31866273b
Jessie Liu <hanyan.liu@outlook.com>
2022-01-23 23:26:19 +1100

    put Atlas DB string back

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 exports.connectToDB = () => {
-  // const { DB_HOST } = process.env;
-  // const connectionString = `${DB_HOST}`;
-  // return mongoose.connect(connectionString);
+  const { DB_HOST } = process.env;
+  const connectionString = `${DB_HOST}`;
+  return mongoose.connect(connectionString);
 
   // Use different DB in different Env
-  const { NODE_ENV, DB_HOST_LOCAL, DB_PORT, DB_DATABASE_LOCAL, DB_HOST } = process.env;
-  if (NODE_ENV === 'development') {
-    connectionString = `mongodb://${DB_HOST_LOCAL}:${DB_PORT}/${DB_DATABASE_LOCAL}`;
-  } else {
-    connectionString = `${DB_HOST}`;
-  }
-  return mongoose.connect(connectionString);
+  // const { NODE_ENV, DB_HOST_LOCAL, DB_PORT, DB_DATABASE_LOCAL, DB_HOST } = process.env;
+  // if (NODE_ENV === 'development') {
+  //   connectionString = `mongodb://${DB_HOST_LOCAL}:${DB_PORT}/${DB_DATABASE_LOCAL}`;
+  // } else {
+  //   connectionString = `${DB_HOST}`;
+  // }
+  // return mongoose.connect(connectionString);
 };
